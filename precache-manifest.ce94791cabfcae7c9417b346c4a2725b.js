22b13f98fb919b437457f991cc9be28cd709dff1
Burhanudin Dwi Prakoso <burhandwi.p@gmail.com>
2019-05-19 11:19:07 +0800

    Updates

---
+++ precache-manifest.ce94791cabfcae7c9417b346c4a2725b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c651567da3b0e9711959ff478a3ec0",
+    "url": "/curriculum-vitae/index.html"
+  },
+  {
+    "revision": "fc449130aef64a5e790c",
+    "url": "/curriculum-vitae/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dbdc869f5137c41fe416",
+    "url": "/curriculum-vitae/static/js/2.ceef510d.chunk.js"
+  },
+  {
+    "revision": "fc449130aef64a5e790c",
+    "url": "/curriculum-vitae/static/js/main.67c01f07.chunk.js"
+  },
+  {
+    "revision": "ba936a51681f02d5816c",
+    "url": "/curriculum-vitae/static/js/runtime~main.b003f3c4.js"
+  }
+]);
